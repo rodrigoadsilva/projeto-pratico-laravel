80a00c1e57096e5f08bfe71bb621c2247c53d1dd
Rodrigo da Silva <raswolf23@gmail.com>
2022-12-20 09:51:08 -0300

    First controller created

---
+++ d
@@ -0,0 +1,6 @@
+  aula1[m
+* [32maula2[m
+  main[m
+  [31mremotes/origin/HEAD[m -> origin/main
+  [31mremotes/origin/aula1[m
+  [31mremotes/origin/main[m
--- estoque/resources/views/layouts/main.blade.php
+++ estoque/resources/views/layouts/main.blade.php
@@ -10,16 +10,38 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
         <!-- CSS Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         
         <!-- CSS da aplicação -->
         <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                          <a class="nav-link" href="/">Lista</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/entrar">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/cadastrar">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
       @yield('content')
       <footer>
         <p>HDC Events &copy; 2020</p>
       </footer>
+
+      <!-- Bootstrap JS-->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  
     </body>
 </html>
